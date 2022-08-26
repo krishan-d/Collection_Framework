40f299ba0da7cb653363fc1ee51eb05e94a27104
Krishan Kumar Jangid <krishan.jangid@outlook.com>
2022-08-26 10:16:09 +0530

    Legacy classes are added!

--- src/com/main/LearnStack.java
+++ src/com/main/LearnStack.java
@@ -1,6 +1,7 @@
 package com.main;
 
 import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Stack;
 import java.util.Vector;
 
@@ -22,14 +23,20 @@ public class LearnStack {
 
         //Vector: Synchronized/Legacy class/100% increment capacity in need
         Vector<Integer> v = new Stack<>();
+        //Vector<Integer> v1 = new Vector<>();
         v.add(1);
         v.add(0);
         v.add(4);
         v.add(2);
         System.out.println(v + "\n");
         //Traverse using Enumeration
+        System.out.println("Using Enumeration to iterate:");
         Enumeration<Integer> enumeration = v.elements();
         while (enumeration.hasMoreElements()) System.out.println(enumeration.nextElement());
+
+        System.out.println("\nUsing Iterator to iterate:");
+        Iterator<Integer> itr = v.iterator();
+        while (itr.hasNext()) System.out.println(itr.next());
         System.out.println();
 
         for (Integer i : v)  System.out.println(i);
---
+++ src/com/main/Legacy_ds/LearnEnumSet.java
@@ -0,0 +1,32 @@
+package com.main.Legacy_ds;
+
+import java.util.EnumSet;
+import java.util.Iterator;
+
+enum days {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
+public class LearnEnumSet {
+
+    public static void main(String[] args) {
+
+        EnumSet<days> set = EnumSet.of(days.TUESDAY, days.WEDNESDAY);
+
+        // Traversing elements
+        Iterator<days> iterator = set.iterator();
+        while (iterator.hasNext()) System.out.println(iterator.next());
+
+        EnumSet<days> set1 = EnumSet.allOf(days.class);
+        System.out.println("Week Days:" + set1);
+
+        EnumSet<days> set2 = EnumSet.noneOf(days.class);
+        System.out.println("Week Days:" + set2);
+
+        EnumSet<days> set3 = EnumSet.copyOf(set);
+        System.out.println("Week Days:" + set3);
+
+        EnumSet<days> set4 = EnumSet.complementOf(set);
+        System.out.println("Week Days:" + set4);
+    }
+}
